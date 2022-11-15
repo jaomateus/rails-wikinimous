2195de7b854f63ecc10f56e232d675b2dd6ab09f
Joao Mateus <jaomateus@gmail.com>
2022-11-15 20:37:31 +0100

    defined index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  name = Faker::Lorem.sentence
+  description = Faker::Lorem.paragraph
+  new_article = Article.new(title: name, content: description)
+  new_article.save
+end
